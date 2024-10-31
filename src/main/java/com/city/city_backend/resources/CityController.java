1213affb536696f430354f458a4b63c8c38e5572
AngeloModanez <angelomodanez30@gmail.com>
2024-10-31 02:08:37 -0300

    :recycle: refactor: Adjust code to Status Code requirements

--- src/main/java/com/city/city_backend/resources/CityController.java
+++ src/main/java/com/city/city_backend/resources/CityController.java
@@ -1,6 +1,7 @@
 package com.city.city_backend.resources;
 
 import java.util.List;
+import java.net.URI;
 
 import com.city.city_backend.services.CityService;
 import com.city.city_backend.entities.City;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class CityController {
@@ -20,28 +23,39 @@ public class CityController {
     @Autowired
     private CityService service;
 
+    // Status code: 200 - Ok
     @GetMapping("cities")
-    public List<City> getCities() {
-        return service.getCities();
+    public ResponseEntity<List<City>> getCities() {
+        return ResponseEntity.ok(service.getCities());
     }
 
+    // Status code: 200 - Ok
     @GetMapping("cities/{id}")
-    public City getCityById(@PathVariable int id) {
-        return service.getCityById(id);
+    public ResponseEntity<City> getCityById(@PathVariable int id) {
+        return ResponseEntity.ok(service.getCityById(id));
     }
 
+    // Status code: 201 - Created
     @PostMapping("cities")
-    public City createCity(@RequestBody City city) {
-        return service.createCity(city);
+    public ResponseEntity<City> createCity(@RequestBody City city) {
+        City newCity = service.createCity(city);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newCity.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(newCity);
     }
 
+    // Status code: 204 - No Content
     @DeleteMapping("cities/{id}")
-    public void deleteById(@PathVariable int id) {
+    public ResponseEntity<Void> deleteById(@PathVariable int id) {
         service.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
+    // Status code: 200 - Ok
     @PutMapping("cities/{id}")
-    public void updateCity(@PathVariable int id, @RequestBody City city) {
+    public ResponseEntity<Void> updateCity(@PathVariable int id, @RequestBody City city) {
         service.updateCity(id, city);
+        return ResponseEntity.ok().build();
     }
 }
